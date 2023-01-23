9ff0a37d18e847216b3b3ca8c90704a0a43a00aa
Alevtina <stochna.alevtina@gmail.com>
2023-01-23 14:14:02 +0200

    [H1] added component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
+import H1 from "./components/H1";
+
 const App = () => {
   return (
-    <div className="app">
+    <div className="app bg-dark">
+      <H1 />
     </div>
   );
 }
---
+++ src/components/H1.tsx
@@ -0,0 +1,7 @@
+const H1 = () => {
+  return (
+    <h1>Crypto Ticker</h1>
+  )
+};
+
+export default H1;
