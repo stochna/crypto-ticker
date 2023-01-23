40d15f580928e5a8c8cf55d331d109090121024d
Alevtina <stochna.alevtina@gmail.com>
2023-01-23 20:46:40 +0200

    [H1] added gsap h1 animation

--- src/components/H1.tsx
+++ src/components/H1.tsx
@@ -1,6 +1,22 @@
+import React from "react";
+import gsap from "gsap";
+import TextPlugin from "gsap/TextPlugin";
+
 const H1 = () => {
+  React.useLayoutEffect(() => {
+    gsap.registerPlugin(TextPlugin);
+
+    gsap.to("#h1", {
+      duration: 1,
+      delay: 5,
+      text: {
+        value: "Use Crypto Ticker"
+    },
+    })
+  });
+
   return (
-    <h1>Crypto Ticker</h1>
+    <h1 id="h1">Get cryptocurrency real-time prices</h1>
   )
 };
 
