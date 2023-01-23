eed9de5cf5cfe06d52e1336db6c1ed36ba5730dd
Alevtina <stochna.alevtina@gmail.com>
2023-01-24 01:45:46 +0200

    [H1] added ScrollTrigger animation

--- src/components/H1.tsx
+++ src/components/H1.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import gsap from "gsap";
 import TextPlugin from "gsap/TextPlugin";
+import ScrollTrigger from "gsap/ScrollTrigger";
 
 const H1 = () => {
   React.useLayoutEffect(() => {
     gsap.registerPlugin(TextPlugin);
+    gsap.registerPlugin(ScrollTrigger);
 
     gsap.to("#h1", {
       duration: 1,
@@ -13,10 +15,19 @@ const H1 = () => {
         value: "Start using <br/> Crypto Ticker"
       }
     });
+
+    gsap.to("#h1", {
+      yPercent: -100,
+      scrollTrigger: {
+        trigger: ".app",
+        start: "top +=100px",
+        scrub: true
+      }
+    });
   });
 
   return (
-    <div className="col-md-7 col">
+    <div className="col-md-8 col">
       <h1 id="h1">Get crypto <br/> real-time prices</h1>
     </div>
   )
