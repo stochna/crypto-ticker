83224f8f433a330b907ea618265f08f713fee016
Alevtina <stochna.alevtina@gmail.com>
2023-01-24 09:35:11 +0200

    [CurrencyList] setup components layout and gsap

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import GsapBg from "./components/GsapBg";
 import H1 from "./components/H1";
+import Clock from "./components/Clock";
+import CurrencyList from "./components/CurrencyList";
 
 const App = () => {
   return (
     <div className="app bg-dark">
       <GsapBg />
 
-      <div className="container py-4">
+      <div className="container ps-md-5 ps-0 header">
         <H1 />
+        <Clock />
+      </div>
+
+      <div className="container content">
+        <CurrencyList />
       </div>
 
     </div>
---
+++ src/components/CurrencyList.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+
+const CurrencyList = () => {
+  React.useLayoutEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+
+    const showList = () => gsap.to(".currency-list", {
+      yPercent: -25,
+      scrollTrigger: {
+        trigger: ".currency-list",
+        scrub: true
+      }
+    });
+
+    showList();
+  }, []);
+
+  return (
+    <div className="row currency-list">
+      <div className="col">
+        <ul className="list">
+          <li className="list-item">A first item</li>
+          <li className="list-item">A second item</li>
+          <li className="list-item">A third item</li>
+          <li className="list-item">A fourth item</li>
+          <li className="list-item">And a fifth one</li>
+        </ul>
+      </div>
+    </div>
+  )
+};
+
+export default CurrencyList;
