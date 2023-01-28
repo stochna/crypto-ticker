7ccb384739b53dc54efdc425378bd99b12bb3914
Alevtina <stochna.alevtina@gmail.com>
2023-01-28 22:17:42 +0200

    [Loader] added component

---
+++ src/components/Loader.tsx
@@ -0,0 +1,9 @@
+const Loader = () => {
+  return (
+    <div className="spinner-border spinner-border-sm text-light" role="status">
+      <span className="visually-hidden">Load...</span>
+    </div>
+  )
+};
+
+export default Loader;
